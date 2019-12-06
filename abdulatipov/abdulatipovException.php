4439a69696a2cab68a5f0d898a85bad92499a89a
429273-1 <429273-1>
2019-12-06 17:40:35 +0300

    fourth commit

---
+++ abdulatipov/abdulatipovException.php
@@ -0,0 +1,6 @@
+<?php
+namespace abdulatipov;
+
+class abdulatipovException extends \Exception {
+
+}
--- index.php
+++ index.php
@@ -7,7 +7,33 @@ include_once("abdulatipov/abdulatipovException.php");
 include_once("abdulatipov/Linear.php");
 include_once("abdulatipov/Square.php");
 include_once("abdulatipov/Log.php");
-abdulatipov\Log::log("roots: " . implode(пываываываы);
-abdulatipov\Log::log("roots: " . implode(екнркеркер);
+$co_arr = [];
+foreach(["a", "b", "c"] as $co) {
+	echo "Enter ".$co.": ";
+	$line = stream_get_line(STDIN, 1024, PHP_EOL);
+	$co_arr[$co] = $line === "" ? 0 : $line;
+}
+$a = $co_arr["a"];
+$b = $co_arr["b"];
+$c = $co_arr["c"];
+//abdulatipov\Log::log("Entered numbers: " . implode(", ", $co_arr));
+try {
+	abdulatipov\Log::log("Equation: $a*x^2 + $b*x + $c = 0");
+	//$s = new abdulatipov\Square($a, $b, $c);	
+	abdulatipov\Log::log("roots: " . implode(" ", abdulatipov\Square::_C()->solve($a, $b, $c)));
+	
+	/*	
+	if (is_array($roots)) {
+		abdulatipov\Log::log("two roots");
+		abdulatipov\Log::log("roots: " . $roots[0] . " " . $roots[1]);
+	} else {
+		abdulatipov\Log::log("one root");
+		abdulatipov\Log::log("root: " . $roots);
+	}
+	*/
+}catch(abdulatipov\abdulatipovException $ex) {
+	abdulatipov\Log::log($ex->getMessage());
+}
+
 abdulatipov\Log::write();
 ?>
